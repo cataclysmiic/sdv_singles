2b9e58c13559e71bb10e942218c32ecbf6251285
Jess Dorman <pickupthesky@gmail.com>
2021-07-08 06:34:33 -0400

    Actually fix tooltips, add media queries

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-})
-
 fetch('https://cataclysmiic.github.io/sdv_singles/villagers.json')
     .then(response => response.json())
     .then(json => {
         json.forEach(villager => {
             const div = document.createElement('div');
-            div.innerHTML = `<div class="card mb-3 rounded-3" style="max-width: 540px;">
+            div.innerHTML = `<div class="singles-card card mb-3 rounded-3 d-block">
                 <div class="row g-0">
-                    <div class="col-md-4 text-center" style="background: #f7f7f7;">
-                    <img src="img/${villager.name}.png" class="img-fluid mx-auto d-block pt-2" alt="${villager.name}">
+                    <div class="col-md-4 text-center ltgrey py-2">
+                    <img src="img/${villager.name}.png" class="img-fluid mx-auto d-block photo" alt="${villager.name}">
                     <h5 class="card-title pt-2">${villager.name}</h5>
                     <span class="badge rounded-pill bg-dark">${villager.bday}</span>
                     </div>
@@ -20,10 +15,10 @@ fetch('https://cataclysmiic.github.io/sdv_singles/villagers.json')
                         <p>“${villager.quote}”</p>
                         <table class="table table-borderless mb-0 text-center">
                             <tr>
-                            <td><span class="badge p-2" style="background: #B33C86;">Loves</span></td>
-                            <td><span class="badge p-2" style="background: #EA638C;">Likes</span></td>
-                            <td><span class="badge p-2" style="background: #C6ECAE;">Dislikes</span></td>
-                            <td><span class="badge p-2" style="background: #94C9A9;">Hates</span></td>
+                            <td><span class="badge p-2 loves">Loves</span></td>
+                            <td><span class="badge p-2 likes">Likes</span></td>
+                            <td><span class="badge p-2 dislikes">Dislikes</span></td>
+                            <td><span class="badge p-2 hates">Hates</span></td>
                             </tr>
                             <tr>
                             <td><img src="img/${villager.loves}.png" data-bs-toggle="tooltip" data-bs-placement="bottom" title="${villager.loves}"></td>
@@ -37,5 +32,12 @@ fetch('https://cataclysmiic.github.io/sdv_singles/villagers.json')
                 </div>
             </div>`;
             document.getElementById('singles').appendChild(div);
+        })
+    })
+    .then(tooltip => {
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl);
         });
     })
+
